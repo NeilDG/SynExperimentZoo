c4c1c0ba9035e24e0d2945f545dbe49d9bc35ee3
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-04-03 13:03:20 +0800

    Update titan1_main.py

--- titan1_main.py
+++ titan1_main.py
@@ -3,18 +3,18 @@
 import os
 
 def train_sr_main():
-    os.system("python \"train_sr_main.py\" --server_config=4 --img_to_load=-1 "
+    os.system("python3 \"train_sr_main.py\" --server_config=4 --img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.01_flickr2k\" --iteration=3")
 
-    os.system("python \"train_sr_main.py\" --server_config=4 --img_to_load=-1 "
+    os.system("python3 \"train_sr_main.py\" --server_config=4 --img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.00_flickr2k\" --iteration=3")
 
 def test_sr_main():
-    os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
+    os.system("python3 \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
               "--plot_enabled=1 --network_version=\"mobisr_v01.00_burstsr\" --iteration=3")
 
 def download_ml_hypersim():
-    os.system("python \"utils/ml_hypersim_dl.py\" --contains scene_cam_00_final_preview --contains .color.jpg")
+    os.system("python3 \"utils/ml_hypersim_dl.py\" --contains scene_cam_00_final_preview --contains .color.jpg")
 
 def main():
     train_sr_main()
