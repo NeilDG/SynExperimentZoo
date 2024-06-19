b687e37ebb7f0684350e1d79dc0c37e6c7f83c9a
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-06-19 17:29:55 +0800

    Update g411_pc1_main.py

--- g411_pc1_main.py
+++ g411_pc1_main.py
@@ -3,41 +3,8 @@
 import os
 
 def train_sr_main():
-    os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.00_burstsr\" --iteration=3")
-
-    os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.01_burstsr\" --iteration=3")
-
-    os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
+    os.system("python \"train_sr_main.py\" --server_config=6 --img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.02_burstsr\" --iteration=3")
-
-    os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.03_burstsr\" --iteration=3")
-
-    os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.04_burstsr\" --iteration=3")
-
-    os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.05_burstsr\" --iteration=3")
-
-    os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.00_burstsr\" --iteration=1")
-
-    os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.00_burstsr\" --iteration=2")
-    #
-    # os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-    #           "--plot_enabled=1 --save_per_iter=250 --network_version=\"mobisr_v01.00_flickr2k\" --iteration=3")
-    #
-    # os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-    #           "--plot_enabled=1 --save_per_iter=250 --network_version=\"mobisr_v01.01_burstsr\" --iteration=3")
-    #
-    # os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-    #           "--plot_enabled=1 --save_per_iter=250 --network_version=\"mobisr_v01.02_burstsr\" --iteration=3")
-    #
-    # os.system("python \"train_sr_main.py\" --server_config=5 --img_to_load=-1 "
-    #           "--plot_enabled=1 --save_per_iter=250 --network_version=\"mobisr_v01.03_burstsr\" --iteration=3")
 
 def test_sr_main():
     os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
