43b34c09ba99ed8a70421d5c537bae08ec129616
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-07-01 18:07:49 +0800

    Update g411_pc2_main.py

--- g411_pc2_main.py
+++ g411_pc2_main.py
@@ -3,19 +3,19 @@
 import os
 
 def train_sr_main():
-    os.system("python \"train_sr_main.py\" --server_config=6 --img_to_load=-1 "
+    os.system("python \"train_sr_main.py\" --server_config=60--img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.05_div2k\" --iteration=6")
 
-    os.system("python \"train_sr_main.py\" --server_config=6 --img_to_load=-1 "
+    os.system("python \"train_sr_main.py\" --server_config=0 --img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.05_div2k\" --iteration=7")
 
-    os.system("python \"train_sr_main.py\" --server_config=6 --img_to_load=-1 "
+    os.system("python \"train_sr_main.py\" --server_config=0 --img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.05_div2k\" --iteration=8")
 
-    os.system("python \"train_sr_main.py\" --server_config=6 --img_to_load=-1 "
+    os.system("python \"train_sr_main.py\" --server_config=0 --img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.05_div2k\" --iteration=9")
 
-    os.system("python \"train_sr_main.py\" --server_config=6 --img_to_load=-1 "
+    os.system("python \"train_sr_main.py\" --server_config=0 --img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.05_div2k\" --iteration=10")
 
 def test_sr_main():
