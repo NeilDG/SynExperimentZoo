d73f8db07f5e5eeae8357124a96aa906e6794ea5
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-07-28 10:36:51 +0800

    Refactored script names and added 4070ti training script.

--- rtx_3090_main.py
+++ rtx_3090_main.py
@@ -25,23 +25,11 @@ def train_sr_main():
     #           "--plot_enabled=1 --save_per_iter=250 --network_version=\"mobisr_v01.00_flickr2k\" --iteration=3")
 
 def test_sr_main():
-    # os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
-    #           "--plot_enabled=0 --network_version=\"mobisr_v01.05_div2k\" --iteration=5")
-    #
-    # os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
-    #           "--plot_enabled=0 --network_version=\"mobisr_v01.05_div2k\" --iteration=6")
-
     os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
-              "--plot_enabled=1 --network_version=\"mobisr_v01.05_div2k\" --iteration=7")
+              "--plot_enabled=0 --network_version=\"mobisr_v01.02_div2k\" --iteration=3")
     #
     # os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
-    #           "--plot_enabled=0 --network_version=\"mobisr_v01.05_div2k\" --iteration=8")
-    #
-    # os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
-    #           "--plot_enabled=0 --network_version=\"mobisr_v01.05_div2k\" --iteration=9")
-
-    os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
-              "--plot_enabled=1 --network_version=\"mobisr_v01.05_div2k\" --iteration=10")
+    #           "--plot_enabled=0 --network_version=\"mobisr_v01.03_div2k\" --iteration=3")
 
 def download_ml_hypersim():
     os.system("python \"utils/ml_hypersim_dl.py\" --contains scene_cam_00_final_preview --contains .color.jpg")
