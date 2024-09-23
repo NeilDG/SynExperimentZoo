8513734e72e581a88a3ddeb73a872887ddd8c0aa
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-09-23 13:02:54 +0800

    Update g411_3060-pc3_main.py

--- g411_3060-pc3_main.py
+++ g411_3060-pc3_main.py
@@ -4,10 +4,10 @@ import os
 
 def train_sr_main():
     os.system("python \"train_sr_main.py\" --server_config=6 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.03_div2k\" --iteration=10")
+              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.11_div2k\" --iteration=10")
 
     os.system("python \"train_sr_main.py\" --server_config=6 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.03_div2k\" --iteration=12")
+              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.11_div2k\" --iteration=12")
 def test_sr_main():
     os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
               "--plot_enabled=1 --network_version=\"mobisr_v01.00_burstsr\" --iteration=3")
