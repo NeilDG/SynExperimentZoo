16c1832f31b6c2efde24941f61beb8d64e97986f
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-09-23 12:38:55 +0800

    Update titan1_3060_main.py

--- titan1_3060_main.py
+++ titan1_3060_main.py
@@ -3,8 +3,8 @@
 import os
 
 def train_sr_main():
-    # os.system("python3 \"train_sr_main.py\" --server_config=4 --img_to_load=-1 "
-    #           "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.06_div2k\" --iteration=1")
+    os.system("python3 \"train_sr_main.py\" --server_config=4 --img_to_load=-1 "
+              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.06_div2k\" --iteration=1")
 
     os.system("python3 \"train_sr_main.py\" --server_config=4 --img_to_load=-1 "
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v01.06_div2k\" --iteration=2")
