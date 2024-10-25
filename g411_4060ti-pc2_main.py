cb68c591d7442a8f85be658889b06186d8a84e8f
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-10-25 18:16:14 +0800

    Update g411_4060ti-pc2_main.py

--- g411_4060ti-pc2_main.py
+++ g411_4060ti-pc2_main.py
@@ -3,8 +3,8 @@
 import os
 
 def train_sr_main():
-    os.system("python \"train_sr_main.py\" --server_config=0 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.02_div2k\" --iteration=1")
+    os.system("python \"train_sr_main.py\" --server_config=7 --img_to_load=-1 "
+              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.05_div2k.01.2")
 
 def test_sr_main():
     os.system("python \"test_sr_main.py\" --server_config=0 --img_to_load=-1 "
