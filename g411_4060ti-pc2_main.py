8a393809c608b1510af6be51b736f0a855a3d1c4
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-11-01 18:03:33 +0800

    Bug fixes in quotes.

--- g411_4060ti-pc2_main.py
+++ g411_4060ti-pc2_main.py
@@ -4,10 +4,10 @@ import os
 
 def train_sr_main():
     os.system("python \"train_sr_main.py\" --server_config=0 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.05_div2k.11.1")
+              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.05_div2k.11.1\"")
 
     os.system("python \"train_sr_main.py\" --server_config=0 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.05_div2k.12.1")
+              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.05_div2k.12.1\"")
 
 def test_sr_main():
     os.system("python \"test_sr_main.py\" --server_config=0 --img_to_load=-1 "
--- titan1_3060_main.py
+++ titan1_3060_main.py
@@ -4,7 +4,7 @@ import os
 
 def train_sr_main():
     os.system("python3 \"train_sr_main.py\" --server_config=4 --img_to_load=-1 "
-              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.05_div2k.05.1")
+              "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.05_div2k.05.1\"")
 
 def test_sr_main():
     os.system("python3 \"test_sr_main.py\" --server_config=4 --img_to_load=-1 "
