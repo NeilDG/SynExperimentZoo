999dc58b6307a9816aab876020d0faa3de630d4e
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2025-03-15 13:20:23 +0800

    Update rl202_rtx3060-pc6_main.py

--- rl202_rtx3060-pc6_main.py
+++ rl202_rtx3060-pc6_main.py
@@ -10,8 +10,22 @@ def train_sr_main():
               "--plot_enabled=0 --save_per_iter=250 --network_version=\"mobisr_v02.05_div2k.09.2\"")
 
 def test_sr_main():
-    os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
+    os.system("python \"test_sr_main.py\" --server_config=7 --img_to_load=-1 "
               "--plot_enabled=1 --network_version=\"mobisr_v01.00_burstsr\" --iteration=3")
+
+def train_seg_main():
+    os.system("python \"train_seg_main_2.py\" --server_config=7 --img_to_load=-1 "
+              "--plot_enabled=1 --save_per_iter=500 --network_version=\"synseg_v00.00_fcg.01.1\"")
+
+
+def test_seg_main():
+    os.system("python \"test_seg_main.py\" --server_config=7 --img_to_load=-1 "
+              "--plot_enabled=1 --network_version=\"synseg_v00.00_cityscapes.01.1\"")
+
+
+def train_img2img_main():
+    os.system("python \"train_img2img_main.py\" --server_config=7 --img_to_load=-1 "
+              "--plot_enabled=1 --save_per_iter=500 --network_version=\"fcg2cityscapes_v00.00.01.1\"")
 
 def download_ml_hypersim():
     os.system("python \"utils/ml_hypersim_dl.py\" --contains scene_cam_00_final_preview --contains .color.jpg")
