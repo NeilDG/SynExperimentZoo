858c3d69b628d12f608ab352def8aa0df3ed6d0b
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2025-03-15 15:59:28 +0800

    Update rl202_rtx3060-pc5_main.py

--- rl202_rtx3060-pc5_main.py
+++ rl202_rtx3060-pc5_main.py
@@ -10,15 +10,22 @@ def train_sr_main():
               "--plot_enabled=1 --save_per_iter=500 --network_version=\"mobisr_v02.05_div2k.08.2\"")
 
 def test_sr_main():
-    os.system("python \"test_sr_main.py\" --server_config=3 --img_to_load=-1 "
+    os.system("python \"test_sr_main.py\" --server_config=7 --img_to_load=-1 "
               "--plot_enabled=1 --network_version=\"mobisr_v01.00_burstsr\" --iteration=3")
+
+def train_img2img_main():
+    os.system("python \"train_img2img_main.py\" --server_config=7 --img_to_load=-1 "
+              "--plot_enabled=1 --save_per_iter=500 --network_version=\"fcg2cityscapes_v00.00.01.2\"")
 
 def download_ml_hypersim():
     os.system("python \"utils/ml_hypersim_dl.py\" --contains scene_cam_00_final_preview --contains .color.jpg")
 
+
+
 def main():
-    train_sr_main()
+    # train_sr_main()
     # test_sr_main()
+    train_img2img_main()
     # download_ml_hypersim()
     # os.system("shutdown /s /t 1")
 
