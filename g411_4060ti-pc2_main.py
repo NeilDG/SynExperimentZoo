b5a134f766334d0097ced357968899355d96c240
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2025-04-04 09:37:25 +0800

    Update g411_4060ti-pc2_main.py

--- g411_4060ti-pc2_main.py
+++ g411_4060ti-pc2_main.py
@@ -32,7 +32,16 @@ def test_seg_main():
 
 def train_img2img_main():
     os.system("python \"train_img2img_main.py\" --server_config=0 --img_to_load=-1 "
-              "--plot_enabled=1 --save_per_iter=500 --network_version=\"fcg2cityscapes_v00.00.02.1\"")
+              "--plot_enabled=1 --save_per_iter=500 --network_version=\"fcg2cityscapes_v00.00.02.3\"")
+
+    os.system("python \"train_img2img_main.py\" --server_config=0 --img_to_load=-1 "
+              "--plot_enabled=1 --save_per_iter=500 --network_version=\"fcg2cityscapes_v00.00.02.4\"")
+
+    os.system("python \"train_img2img_main.py\" --server_config=0 --img_to_load=-1 "
+              "--plot_enabled=1 --save_per_iter=500 --network_version=\"fcg2cityscapes_v00.00.02.5\"")
+
+    os.system("python \"train_img2img_main.py\" --server_config=0 --img_to_load=-1 "
+              "--plot_enabled=1 --save_per_iter=500 --network_version=\"fcg2cityscapes_v00.00.02.6\"")
 
 def run_util_script_main():
     os.system("python \"util_script_main.py\"")
